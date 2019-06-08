fd96e9c1f566cee6046bb5c3f4cb23d2adf2430d
Steven <sgotting21@yahoo.com>
2019-06-08 01:28:24 -0400

    Convert timer seconds function into one liner using sprintf formatting

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -6,19 +6,6 @@ class Timer
   end
 
   def seconds=(seconds)
-    hours = seconds/3600
-    minutes = seconds%3600/60
-    seconds = seconds%3600%60
-    divisions = [hours.to_s, minutes.to_s, seconds.to_s]
-    new_seconds = []
-    divisions.each { |div|
-      if div.length == 1
-        new_seconds.append("0#{div}")
-      else 
-        new_seconds.append(div)
-      end
-      }
-    @time_string = "#{new_seconds[0]}:#{new_seconds[1]}:#{new_seconds[2]}"
+    @time_string = "%02i:%02i:%02i" % [seconds/3600,seconds%3600/60,seconds%3600%60]
     end
-
 end
